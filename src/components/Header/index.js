d34321a5bc0e0955a6a608a447979d8e51582e81
Flavio Ever <flavio.ever@icloud.com>
2019-12-15 12:01:30 -0300

    Header Styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
-import { Container, Logotipo } from './styles';
+import { Container, Logotipo, Cart } from './styles';
 
 export default function Header() {
   return (
@@ -10,8 +11,12 @@ export default function Header() {
         <Logotipo>Redux Shoes</Logotipo>
       </Link>
 
-      <Cart>
-
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 Itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
       </Cart>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,8 +1,39 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const Container = styled.div``;
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
 
 export const Logotipo = styled.span`
   font-size: 32px;
   color: #fff;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  transition: opacity 0.2;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #999;
+    }
+  }
 `;
