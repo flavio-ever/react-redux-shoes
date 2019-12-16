116662e88442d577bb515a5ac2b8f204cfe83435
Flavio Ever <flavio.ever@icloud.com>
2019-12-16 20:58:58 -0300

    Reactotron Enhancer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+// Antes do store
+import './config/reactotronConfig';
+
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
