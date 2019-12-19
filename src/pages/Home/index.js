5360f5f31a318561c1b1424a772c8a060ad8088f
Flavio Ever <flavio.ever@icloud.com>
2019-12-19 12:18:32 -0300

    Amount Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -32,6 +32,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -46,6 +47,7 @@ class Home extends Component {
             >
               <div>
                 <MdShoppingCart size={16} color="#fff" />
+                <span>{amount[product.id] || 0}</span>
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -56,7 +58,14 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
