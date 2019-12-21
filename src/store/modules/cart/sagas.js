5b7c1f4d3662163da59b33a3b21698b08632d73a
Flavio Ever <flavio.ever@icloud.com>
2019-12-21 12:13:47 -0300

    Redux-Saga Update

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -5,7 +5,7 @@ import api from '../../../services/api';
 
 import formatPrice from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExist = yield select(state => state.cart.find(p => p.id === id));
@@ -23,9 +23,7 @@ function* addToCart({ id }) {
   }
 
   if (productExist) {
-    const amount = productExist.amount + 1;
-
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -39,4 +37,19 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada no limite do estoque!');
+  }
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
